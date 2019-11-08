92b27fd0f601447c9b5723252406e48dfd5aa44b
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-08 16:02:17 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs 
+    Song.all.select {|s| s.genre == self}
+  end
+  
+  def artists 
+    songs.map(&:artist)
+  end
+  
+  private
+  def save
+    @@all << self
+end 
