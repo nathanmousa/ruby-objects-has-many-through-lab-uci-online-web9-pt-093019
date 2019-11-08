4b1a8c3a5c2e62a47d7d063285981d82ad2c293a
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-08 16:14:14 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,32 @@
+class Patient
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+  
+  def appointments
+    Appointment.all.select { |app| app.doctor == self }
+  end
+  
+  def patients
+    appointments.collect { |app| app.patient }.uniq
+  end
+  
+  
+  
+  private
+  def save
+    @@all << self
+  end
+end
